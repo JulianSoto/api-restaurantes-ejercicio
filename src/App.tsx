b4602b3d08164f9aaafb60abc4af058b4b4f151f
Julian Soto <juliansotoponce@gmail.com>
2021-10-07 22:09:25 -0700

    use Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch></Switch>
+    </div>
+  );
 }
 
 export default App;
